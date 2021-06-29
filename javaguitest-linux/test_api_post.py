bb141d28c191708bce7a1b98eb6be6e8be78d440
kmlee0922 <lkm0922@gmail.com>
2021-06-29 11:32:36 +0900

    test2

--- javaguitest-linux/test_api_post.py
+++ javaguitest-linux/test_api_post.py
@@ -6,15 +6,57 @@ import json
 url_items = "http://office-api.hubilon.kr/equip/update"
 #response = requests.get(url_items)
 headers = {
-      "accept": "application/json;charset=UTF-8",
-
-      "userId": "admin",
-
-      "Content-Type": "application/json;charset=UTF-8"
+  "accept": "application/json;charset=UTF-8",
+  "userId": "admin",
+  "Content-Type": "application/json;charset=UTF-8"
 }
 
-newItem =  {
-      "paramList": 
+newItem = {
+
+  "message": {
+
+    "header": {
+
+      "userId": "82047554",
+
+      "userPasswd": "",
+
+      "userName": "LDAP테스트4",
+
+      "telNo": "01074451945",
+
+      "userCellPhoneNo": "01074451945",
+
+      "orgId": "C361443",
+
+      "orgName": "양평고객기술팀",
+
+      "orgTypeCode": "INS_USER",
+
+      "job_equnr": "",
+
+      "currentPageNo": "",
+
+      "currentPageCount": "",
+
+      "pageUseYN": "",
+
+      "sessionId": "4C5nnrmjsVKsbZWSjkk8Dsfi.nbase01",
+
+      "orgCode": "361443"
+
+    },
+
+    "body": {
+
+      "param": {
+
+       
+
+      },
+
+      "paramList": [
+
         {
 
           "WORKID": "0006",
@@ -25,9 +67,12 @@ newItem =  {
 
           "PDAUSER": "82047554"
 
-        },
+        }
 
-      "subParamList":
+      ],
+
+      "subParamList": [
+
         {
 
           "PARTTYPE": "30",
@@ -47,10 +92,158 @@ newItem =  {
           "ZPSTATU": "0080",
 
           "MAKTX": "LUT_DACB(DAC100)"
+
         }
+
+      ]
+
+    },
+
+    "call": "ANDROID",
+
+    "jobSeq": "1",
+
+    "operatorId": "",
+
+    "runProgramId": "",
+
+    "sysRegisterDate": {
+
+     
+
+    },
+
+    "sysUpdateDate": {
+
+     
+
+    }
+
+  }
+
 }
-    
 
-response = requests.post(url_items, headers = headers ,data=newItem)
 
-print(response.text)
+newItem2 = {
+
+  "message": {
+
+    "header": {
+
+      "userId": "82047554",
+
+      "userPasswd": "",
+
+      "userName": "LDAP테스트4",
+
+      "telNo": "01074451945",
+
+      "userCellPhoneNo": "01074451945",
+
+      "orgId": "C361443",
+
+      "orgName": "양평고객기술팀",
+
+      "orgTypeCode": "INS_USER",
+
+      "job_equnr": "",
+
+      "currentPageNo": "",
+
+      "currentPageCount": "",
+
+      "pageUseYN": "",
+
+      "sessionId": "4C5nnrmjsVKsbZWSjkk8Dsfi.nbase01",
+
+      "orgCode": "361443"
+
+    },
+
+    "body": {
+
+      "param": {
+
+       
+
+      },
+
+      "paramList": [
+
+        {
+
+          "WORKID": "0006",
+
+          "PRCID": "0035",
+
+          "CHKSCAN": "X",
+
+          "PDAUSER": "82047554"
+
+        }
+
+      ],
+
+      "subParamList": [
+
+        {
+
+          "PARTTYPE": "30",
+
+          "DEVTYPE": "40",
+
+          "EXBARCODE": "",
+
+          "BARCODE": "5E2K9001880003131",
+
+          "DEVICEID": "000000132",
+
+          "SCAN": "1",
+
+          "CHKZKOSTL": "",
+
+          "ZPSTATU": "0080",
+
+          "MAKTX": "LUT_DACB(DAC100)"
+
+        }
+
+      ]
+
+    },
+
+    "call": "ANDROID",
+
+    "jobSeq": "1",
+
+    "operatorId": "",
+
+    "runProgramId": "",
+
+    "sysRegisterDate": {
+
+     
+
+    },
+
+    "sysUpdateDate": {
+
+     
+
+    }
+
+  }
+
+}
+
+def out(equip_barcode):
+  newItem2['message']['body']['subParamList'][0]['BARCODE']=equip_barcode
+  response = requests.post(url_items, headers = headers ,data=json.dumps(newItem2))
+
+
+out("123456")
+# print(newItem)
+
+# response = requests.post(url_items, headers = headers ,data=json.dumps(newItem))
+
+# print(response.text)
